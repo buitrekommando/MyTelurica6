0de5cbb8cf7aea6983c75122935371804cc96933
Jose Luis Henriquez <jl.henriquez@gmail.com>
2023-10-06 20:43:45 -0300

    Revert "ingrese boton."
    This reverts commit 20dd03303ba29539433a030dda0853b0ff76ea3a.

--- app/src/main/java/com/joseluishenriquez/mytelurica6/MainActivity.kt
+++ app/src/main/java/com/joseluishenriquez/mytelurica6/MainActivity.kt
@@ -12,7 +12,6 @@ import javax.security.auth.callback.Callback
 
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
